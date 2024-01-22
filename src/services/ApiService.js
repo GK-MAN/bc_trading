c3ad5abefd7eda1a4e3ff8099751747a10cc3535
Gomolemo Kekana <u17389349@tuks.co.za>
2024-01-22 13:26:35 +0200

    Added the sell endpoint

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -11,10 +11,24 @@ const ApiService = {
         return response.data;
     },
     
-    buyContract: async (buyData) => {
+    postBuyContract: async (buyData) => {
         //Buy a Contract
         //https://api.deriv.com/api-explorer#buy
         const response = await axios.post(`${API_BASE_URL}/buy_contract`, buyData);
+        return response.data;
+    },
+
+    postPortfolio: async (portfolioData) => {
+        //Receive information about my current portfolio of outstanding options
+        //https://api.deriv.com/api-explorer#portfolio
+        const response = await axios.post(`${API_BASE_URL}/portfolio`, portfolioData);
+        return response.data;
+    },
+
+    postSellContract: async (sellData) => {
+        //Sell a Contract as identified from a previous `portfolio` call.
+        //https://api.deriv.com/api-explorer#sell
+        const response = await axios.post(`${API_BASE_URL}/sell_contract`, sellData);
         return response.data;
     },
 
